0a197ee729288048f902b0b26c8848eacee5a8b3
sh1ft <pmayzenberg@gmail.com>
2019-08-22 15:40:40 +0300

    Add project files.

---
+++ SignatureCheckerV2/Program.cs
@@ -0,0 +1,171 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using iTextSharp.text.pdf;
+
+namespace SignatureCheckerV2
+{
+    class Program
+    {
+        private static IEnumerable<string> filenames;
+        private static string srcFolder;
+        private static string dstFolder;
+        private static string currentDate = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
+        private static int row = 1;
+        private static Application excel;
+        private static Workbook wb;
+        private static Worksheet ws;
+        private static string resultFile;
+
+        private static void DisplayHelp()
+        {
+            Console.WriteLine("Program Usage:");
+            Console.WriteLine("SignatureCheckerV2 <Source folder> <Output file save path>");
+            /* Console.WriteLine("\t-s --source\tSource directory path");
+             Console.WriteLine("\t-d --source\tDestination directory path");*/
+            Console.WriteLine("\t-p --prompt\tPrompt mode. You will provide source and destination path through prompt");
+            Console.WriteLine("\t-h --help\tShow this help");
+
+        }
+        static void Main(string[] args)
+        {
+
+            //string resultFile = Directory.GetCurrentDirectory().ToString() + @"\Result_" + currentDate + ".xlsx";
+            try
+            {
+                if (args.Length == 0)
+                {
+                    DisplayHelp();
+                    System.Environment.Exit(0);
+
+                }
+                else if (args.Length > 2)
+                {
+                    Console.WriteLine("Invalid number of arguments!");
+                    System.Environment.Exit(1);
+                }
+                else if (args.Length == 2)
+                {
+                    //if (args[0].ToLower().Equals("-s") || args[2].ToLower().Equals("-s"))
+                    srcFolder = args[0];
+                    dstFolder = args[1];
+                }
+                else if (args.Length == 1)
+                {
+                    if (args[0].ToLower().Equals("-h") || args[0].ToLower().Equals("--help"))
+                    {
+                        DisplayHelp();
+                    }
+                    else if (args[0].ToLower().Equals("-p") || args[0].ToLower().Equals("--prompt"))
+                    {
+
+                        Console.Write("Enter source folder path. Type \"Exit\" to exit the program: ");
+                        do
+                        {
+                            srcFolder = Console.ReadLine();
+
+                            if (srcFolder.ToLower().Equals("exit"))
+                            {
+                                Console.WriteLine("Exiting");
+                                System.Environment.Exit(0);
+                            }
+
+                            if (srcFolder.Length == 0)
+                            {
+                                Console.WriteLine("Source Folder Path cannot be empty!");
+                                Console.Write("Enter source folder path. Type \"Exit\" to exit the program: ");
+                            }
+
+                        } while (srcFolder.Length == 0);
+                    }
+
+
+                    Console.Write("Destination Directory (Press Enter to create the file in default location):");
+                    dstFolder = Console.ReadLine();
+
+                    if (dstFolder.Length == 0)
+                    {
+                        dstFolder = @"C:\Temp";
+                        Console.WriteLine("The file will be created in \"C:\\Temp\"");
+                    }
+
+
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid argument!");
+                    System.Environment.Exit(1);
+                }
+
+
+                if (!Directory.Exists(dstFolder))
+                {
+                    Directory.CreateDirectory(dstFolder);
+                }
+
+                resultFile = dstFolder + @"\Result_" + currentDate + ".xlsx";
+
+                // Creating excel file
+                excel = new Application { Visible = false };
+                wb = excel.Workbooks.Add(Type.Missing);
+                ws = wb.ActiveSheet;
+                ws.Name = "Result";
+
+                // Set first row headers to filter later
+                ws.Cells[row, 1] = "Filename";
+                ws.Cells[row, 2] = "Signing Status";
+                row++;
+
+
+                // Get all target filenames from directory including sub-directories               
+                filenames = Directory.EnumerateFiles(srcFolder, "*.pdf", SearchOption.AllDirectories);
+
+                foreach (string filename in filenames)
+                {
+
+                    using (var doc = new PdfReader(filename))
+                    {
+
+                        AcroFields acroFields = doc.AcroFields;
+
+                        // Checking if signature fields exist
+                        if (acroFields.GetSignatureNames().Count != 0)
+                        {
+
+
+                            ws.Cells[row, 1] = filename;
+                            ws.Cells[row, 2] = "signed";
+                            row++;
+                        }
+                        else
+                        {
+                            ws.Cells[row, 1] = filename;
+                            ws.Cells[row, 2] = "unsigned";
+                            row++;
+
+                        }
+
+                    }
+                }
+
+                // Adding Autofilter
+                
+                ws.UsedRange.AutoFilter(1, Type.Missing, XlAutoFilterOperator.xlAnd, Type.Missing, true);
+                ws.Columns.AutoFit();
+                wb.SaveAs(resultFile);
+
+                excel.Visible = true;
+
+                
+
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ SignatureCheckerV2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SignatureCheckerV2")]
+[assembly: AssemblyDescription("Checks if digital signature exists in pdf file")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CBC Group")]
+[assembly: AssemblyProduct("SignatureCheckerV2")]
+[assembly: AssemblyCopyright("Copyright ©  2019 Pavel Mayzenberg")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e7f3fd3b-5087-44c3-bbab-8f57d9609a12")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
